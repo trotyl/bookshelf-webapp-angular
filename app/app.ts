c7a2e4413e4c5cd09e9c31bb276d44d386993afe
Trotyl Yu <yzj1995@vip.qq.com>
2016-01-01 20:31:54 +0800

    [Trotyl] Create template of component Navbar.

--- app/app.ts
+++ app/app.ts
@@ -1,10 +1,13 @@
 import { Component } from 'angular2/core';
+import { Navbar } from 'app/navbar';
 
 @Component({
     selector: 'app',
     template: `
+        <navbar></navbar>
         <h1>Trotyl's Bookshelf</h1>
-    `
+    `,
+    directives: [ Navbar ]
 })
 export class App {
 
---
+++ app/navbar.ts
@@ -0,0 +1,54 @@
+import { Component } from 'angular2/core';
+
+@Component({
+    selector: 'navbar',
+    template: `
+        <nav class="navbar navbar-default">
+            <div class="container-fluid">
+                <!-- Brand and toggle get grouped for better mobile display -->
+                <div class="navbar-header">
+                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <a class="navbar-brand" href="#">Trotyl's Bookshelf</a>
+                </div>
+
+                <!-- Collect the nav links, forms, and other content for toggling -->
+                <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                    <ul class="nav navbar-nav">
+                        <li class="active"><a href="#">Home <span class="sr-only">(current)</span></a></li>
+                        <li><a href="#">Create Book</a></li>
+                        <li class="dropdown">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Categories <span class="caret"></span></a>
+                            <ul class="dropdown-menu">
+                                <li><a href="#">Arts & Photography</a></li>
+                                <li><a href="#">Biographies & Memoirs</a></li>
+                                <li><a href="#">Business & Money</a></li>
+                                <li><a href="#">Computer & Technology</a></li>
+                                <li role="separator" class="divider"></li>
+                                <li><a href="#">All</a></li>
+                            </ul>
+                        </li>
+                    </ul>
+                    <form class="navbar-form navbar-left" role="search">
+                        <div class="form-group">
+                            <input type="text" class="form-control" placeholder="CLR via C#">
+                        </div>
+                        <button type="submit" class="btn btn-default">Search</button>
+                    </form>
+                    <ul class="nav navbar-nav navbar-right">
+                        <li><a href="#">Set Default</a></li>
+                        <li><a href="#">Favorite</a></li>
+                        <li><a href="#">Help</a></li>
+                    </ul>
+                </div><!-- /.navbar-collapse -->
+            </div><!-- /.container-fluid -->
+        </nav>
+    `
+})
+export class Navbar {
+
+}
