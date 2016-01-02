b8ef05bce0ec945a83071fab1f78122ecb560f79
Trotyl Yu <yzj1995@vip.qq.com>
2016-01-02 15:28:59 +0800

    [Trotyl] Add fake pagination for BookList.

--- app/components/book-list.component.ts
+++ app/components/book-list.component.ts
@@ -27,6 +27,25 @@ import { Book } from '../models/book';
                 </tr>
             </tbody>
         </table>
+        <nav class="text-center">
+          <ul class="pagination">
+            <li>
+              <a href="#" aria-label="Previous">
+                <span aria-hidden="true">&laquo;</span>
+              </a>
+            </li>
+            <li><a href="#">1</a></li>
+            <li><a href="#">2</a></li>
+            <li><a href="#">3</a></li>
+            <li><a href="#">4</a></li>
+            <li><a href="#">5</a></li>
+            <li>
+              <a href="#" aria-label="Next">
+                <span aria-hidden="true">&raquo;</span>
+              </a>
+            </li>
+          </ul>
+        </nav>
     `,
     directives: [ NgFor ]
 })
