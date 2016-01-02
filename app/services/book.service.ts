9105c616d01b1998ec3e0ecb85f5795539cf9c5b
Trotyl Yu <yzj1995@vip.qq.com>
2016-01-02 23:45:47 +0800

    [Trotyl] Change api structure to flatten models.

--- app/services/book.service.ts
+++ app/services/book.service.ts
@@ -2,25 +2,46 @@ import { Injectable } from 'angular2/core';
 import { Http } from 'angular2/http';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
-import 'rxjs/add/operator/delay';
+import 'rxjs/add/operator/zip-static';
+import 'rxjs/add/operator/mergeMap';
 import { Book } from "../models/book";
 import { Category } from '../models/category';
 import { CategoryService } from './category.service';
 
 @Injectable()
 export class BookService {
-    private books: Observable<Book[]>;
+    private booksObservable: Observable<Book[]>;
 
     constructor(private http: Http, private categoryService: CategoryService) {
-        this.books = http.get('/api/books')
-            .map(res => res.json());
+        this.booksObservable = http.get('/api/books')
+            .map(res => res.json())
+            .mergeMap(array => this.parseBooks(array));
     }
 
     getBooks(start: number = 0, amount: number = 10): Observable<Book[]> {
-        return this.books.map(books => books.filter((_, i) => i >= start && i < start + amount));
+        return this.booksObservable.map(books => books.filter((_, i) => i >= start && i < start + amount));
     }
 
     getNumberOfBooks(): Observable<number> {
-        return this.books.map(books => books.length);
+        return this.booksObservable.map(books => books.length);
     }
+
+    parseBooks(array: BookJson[]): Observable<Book[]> {
+        return Observable.zip(...array.map(json => this.categoryService.getCategory(json.categoryId)),
+            (...categories) => categories).map(categories => categories.map((category, i) => ({
+                isbn: array[i].isbn,
+                title: array[i].title,
+                author: array[i].author,
+                category: category,
+                price: array[i].price
+            })));
+    }
+}
+
+interface BookJson {
+    isbn: string,
+    title: string,
+    author: string[],
+    categoryId: string,
+    price: number
 }
--- app/services/category.service.ts
+++ app/services/category.service.ts
@@ -1,11 +1,15 @@
 import { Injectable } from 'angular2/core';
 import { Http } from 'angular2/http';
 import { Observable } from 'rxjs/Observable';
-import 'rxjs/add/operator/map';
 import { Book } from "../models/book";
 import { Category } from '../models/category';
 
 @Injectable()
 export class CategoryService {
-
+    getCategory(id: string): Observable<Category> {
+        return Observable.create((observer) => observer.next({
+            id: id,
+            name: 'Computer & Technology'
+        }));
+    }
 }
