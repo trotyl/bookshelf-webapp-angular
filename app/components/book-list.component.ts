4bb15ff70bdc9fe9b15b60305a97d648cffe7483
Trotyl Yu <yzj1995@vip.qq.com>
2016-01-02 20:18:03 +0800

    [Trotyl] Seperate PaginationComponent from BookListComponent.

--- app/components/book-list.component.ts
+++ app/components/book-list.component.ts
@@ -1,11 +1,10 @@
 import { Component, Input } from 'angular2/core';
 import { COMMON_DIRECTIVES } from 'angular2/common';
-import { Router, RouterLink, Location, RouteParams, RouteConfig } from 'angular2/router';
+import { RouteParams } from 'angular2/router';
 import { Observable } from 'rxjs/Observable';
+import { PaginationComponent } from './pagination.component';
 import { BookService } from '../services/book.service';
 import { Book } from '../models/book';
-import { RangePipe } from '../pipes/range.pipe';
-
 
 @Component({
     selector: 'book-list',
@@ -29,37 +28,19 @@ import { RangePipe } from '../pipes/range.pipe';
                 </tr>
             </tbody>
         </table>
-        <nav class="text-center">
-          <ul class="pagination">
-            <li>
-              <a href="#" aria-label="Previous">
-                <span aria-hidden="true">&laquo;</span>
-              </a>
-            </li>
-            <li *ngFor="#page of pages | range:1">
-                <a *ngIf="page == 1" [routerLink]="['BookList']">{{ page }}</a>
-                <a *ngIf="page > 1" [routerLink]="['BookListPage', { page: page }]">{{ page }}</a>
-            </li>
-            <li>
-              <a href="#" aria-label="Next">
-                <span aria-hidden="true">&raquo;</span>
-              </a>
-            </li>
-          </ul>
-        </nav>
+        <pagination [current]="currentPage" [total]="pages"></pagination>
     `,
-    directives: [ COMMON_DIRECTIVES, RouterLink ],
-    pipes: [ RangePipe ]
+    directives: [ COMMON_DIRECTIVES, PaginationComponent ]
 })
 export class BookListComponent {
     private books: Book[];
     private pages: number;
     private currentPage: number;
 
-    constructor(private router: Router, private location: Location, private params: RouteParams, private bookService: BookService) {
-        var page = params.get('page') || 1;
+    constructor(private params: RouteParams, private bookService: BookService) {
+        this.currentPage = params.get('page') as number || 1;
 
-        bookService.getBooks((page as number - 1) * 10).subscribe(books => this.books = books);
+        bookService.getBooks((this.currentPage - 1) * 10).subscribe(books => this.books = books);
         bookService.getNumberOfBooks().subscribe(num => this.pages = Math.floor(num / 10) + 1);
     }
 }
---
+++ app/components/pagination.component.ts
@@ -0,0 +1,33 @@
+import { Component, Input } from 'angular2/core';
+import { COMMON_DIRECTIVES } from 'angular2/common';
+import { ROUTER_DIRECTIVES } from 'angular2/router';
+import { RangePipe } from '../pipes/range.pipe';
+
+@Component({
+    selector: 'pagination',
+    template: `
+        <nav class="text-center">
+          <ul class="pagination">
+            <li [ngClass]="{ disabled: current <= 1 }">
+              <a [routerLink]="current > 2 ? ['BookListPage', { page: page }] : ['BookList']" aria-label="Previous">
+                <span aria-hidden="true">&laquo;</span>
+              </a>
+            </li>
+            <li *ngFor="#page of total | range:1" [ngClass]="{ active: page == current }" (click)="current = page">
+                <a [routerLink]="page > 1 ? ['BookListPage', { page: page }] : ['BookList']">{{ page }}</a>
+            </li>
+            <li [ngClass]="{ disabled: current >= total }">
+              <a href="#" aria-label="Next">
+                <span aria-hidden="true">&raquo;</span>
+              </a>
+            </li>
+          </ul>
+        </nav>
+    `,
+    directives: [ COMMON_DIRECTIVES, ROUTER_DIRECTIVES ],
+    pipes: [ RangePipe ]
+})
+export class PaginationComponent {
+    @Input() private current: number;
+    @Input() private total: number;
+}
