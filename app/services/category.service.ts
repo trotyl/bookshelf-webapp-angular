90b09fe2dfa6f3384aeab9b73e2c6ed54162f94c
Trotyl Yu <yzj1995@vip.qq.com>
2016-01-03 13:16:36 +0800

    [Trotyl] Add support for cache in CategoryService.

--- app/services/category.service.ts
+++ app/services/category.service.ts
@@ -1,16 +1,21 @@
 import { Injectable } from 'angular2/core';
 import { Http } from 'angular2/http';
 import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/do';
+import 'rxjs/add/operator/catch';
 import { Book } from "../models/book";
 import { Category } from '../models/category';
 
 @Injectable()
 export class CategoryService {
     private observableCategories: Observable<Category[]>;
+    private cachedCategories: Map<string, Category>;
 
     constructor(private http: Http) {
+        this.cachedCategories = new Map<string, Category>();
         this.observableCategories = http.get('/api/categories')
             .map(res => res.json())
+            .do(categories => categories.forEach(category => this.cachedCategories.set(category.id, category)));
     }
 
     getCategories(): Observable<Category[]> {
@@ -18,6 +23,12 @@ export class CategoryService {
     }
 
     getCategory(id: string): Observable<Category> {
-        return this.observableCategories.map(categories => categories.find(category => category.id == id));
+        return Observable.create(observer => {
+            if (this.cachedCategories.has(id)) {
+                observer.next(this.cachedCategories.get(id));
+            } else {
+                observer.error('Category not found in cache.');
+            }
+        }).catch(this.observableCategories.map(categories => categories.find(category => category.id == id)));
     }
 }
