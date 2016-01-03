5184b3ac6643593ba1d66526ff6380c0402b1753
Trotyl Yu <yzj1995@vip.qq.com>
2016-01-04 00:12:13 +0800

    [Trotyl] Refactor RangePipe to use destruct of ES6.

--- app/pipes/range.pipe.ts
+++ app/pipes/range.pipe.ts
@@ -4,9 +4,8 @@ import { Pipe } from 'angular2/core';
     name: 'range'
 })
 export class RangePipe {
-    transform(value: number, args: number[]): number[] {
+    transform(value: number, [ start = 0, ...rest ]): number[] {
         var result: number[] = [];
-        let start = args[0] || 0;
         var i = 0;
         while(i < value) {
             result.push(i + start);
