4359e96ee56c33287d97e1d3c90faf4506be4538
Trotyl Yu <yzj1995@vip.qq.com>
2016-01-06 13:24:58 +0800

    [Trotyl] Remove unnecessary dependency of BookDetailComponnet.

--- app/components/book-detail.component.ts
+++ app/components/book-detail.component.ts
@@ -1,12 +1,7 @@
 import { Component } from 'angular2/core';
-import { FORM_DIRECTIVES } from 'angular2/common';
 import { RouteParams } from 'angular2/router';
-import { Observable } from 'rxjs/Rx';
 import { BookEditComponent } from './book-edit.component';
 import { BookFormComponent } from './book-form.component';
-import { BookService } from '../services/services';
-import { Book } from '../models/models';
-import { ListPipe } from '../pipes/pipes';
 
 @Component({
     selector: 'book-detail',
@@ -14,13 +9,12 @@ import { ListPipe } from '../pipes/pipes';
         <div class="page-header"><h2>Book Detail</h2></div>
         <book-form [isbn]="isbn" [disabled]="true"></book-form>
     `,
-    directives: [ FORM_DIRECTIVES, BookFormComponent ],
-    pipes: [ ListPipe ]
+    directives: [ BookFormComponent ]
 })
 export class BookDetailComponent {
     private isbn: string;
 
-    constructor(private routeParams: RouteParams, private bookService: BookService) {
+    constructor(private routeParams: RouteParams) {
         this.isbn = routeParams.get('isbn');
     }
 }
