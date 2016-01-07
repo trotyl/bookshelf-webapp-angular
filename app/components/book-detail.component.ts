16125076bf1a6d1b6609efd354c61b8fcff5d997
Trotyl Yu <yzj1995@vip.qq.com>
2016-01-07 21:29:37 +0800

    [Trotyl] Fixed BookDetailComponent for transfer book directly.

--- app/components/book-detail.component.ts
+++ app/components/book-detail.component.ts
@@ -1,20 +1,29 @@
-import { Component } from 'angular2/core';
+import { Component, OnInit } from 'angular2/core';
 import { RouteParams } from 'angular2/router';
 import { BookEditComponent } from './book-edit.component';
 import { BookFormComponent } from './book-form.component';
+import { Book } from "../models/book";
+import { BookService } from "../services/book.service";
 
 @Component({
     selector: 'book-detail',
     template: `
         <div class="page-header"><h2>Book Detail</h2></div>
-        <book-form [isbn]="isbn" [editable]="false"></book-form>
+        <book-form [book]="book" [editable]="false"></book-form>
     `,
     directives: [ BookFormComponent ]
 })
-export class BookDetailComponent {
-    private isbn: string;
+export class BookDetailComponent implements OnInit {
 
-    constructor(private routeParams: RouteParams) {
-        this.isbn = routeParams.get('isbn');
+    private book: Book = Book.empty();
+
+    constructor(
+        private routeParams: RouteParams,
+        private bookService: BookService
+    ) { }
+
+    ngOnInit(): void {
+        let isbn = this.routeParams.get('isbn');
+        this.bookService.getBook(isbn).subscribe(book => this.book = book);
     }
 }
